eb538b7f203de965f51c3477e2dfac3b04f0876e
6Scan <6Scan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-12 09:02:51 +0000

    Support for WP 3.5 Registration process changed
    git-svn-id: https://plugins.svn.wordpress.org/6scan-backup/trunk@637584 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 6scan.php
+++ 6scan.php
@@ -4,7 +4,7 @@ Plugin Name: 6Scan Backup
 Plugin URI: http://www.6scan.com/
 Description: 6Scan Backup goes beyond existing backup plugins, easily and automatically creating backups of your site and securely storing them in the cloud.
 Author: 6Scan
-Version: 2.2.9
+Version: 2.3.0
 Author URI: http://www.6scan.com
 */
 
